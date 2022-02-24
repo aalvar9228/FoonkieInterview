3c1d156600b9159dda07ef1e87364ff5ff81c5ac
aalvar9228 <alejoac.personal@gmail.com>
2022-02-24 03:48:02 -0500

    Adding fonts to application

--- Code/FoonkieInterview/FoonkieInterview/AssemblyInfo.cs
+++ Code/FoonkieInterview/FoonkieInterview/AssemblyInfo.cs
@@ -1,3 +1,17 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-[assembly: XamlCompilation(XamlCompilationOptions.Compile)] 
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+#region Rubik Fonts
+
+[assembly: ExportFont("Rubik-Regular.ttf", Alias = "RubikRegular")]
+[assembly: ExportFont("Rubik-Bold.ttf", Alias = "RubikBold")]
+
+#endregion
+
+#region DM Serif Display Fonts
+
+[assembly: ExportFont("DMSerifDisplay-Regular.ttf", Alias = "DMSerifDisplayRegular")]
+
+#endregion
